442013f2babdd1b611932a19ae725d5685d7a395
axiel7 <axiel7lopez@gmail.com>
2020-04-12 10:54:27 +0200

    Added toolbar on settings activity

--- app/src/main/java/com/axiel7/tioanime/SettingsActivity.java
+++ app/src/main/java/com/axiel7/tioanime/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -25,13 +26,18 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
                 .beginTransaction()
                 .replace(R.id.settings, settingsFragment)
                 .commit();
+
+        //setup toolbar
+        Toolbar toolbar = findViewById(R.id.setting_toolbar);
+        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
         }
-        //change status and nav bar color
+
+        //change nav bar color
         getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 
         //setup preferences
         preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
