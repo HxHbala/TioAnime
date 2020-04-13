b00a9235cd20ffedb4542c5f2df5d1a19fa50ba3
axiel7 <axiel7lopez@gmail.com>
2020-04-13 19:52:00 +0200

    Drawer header improvements

--- app/src/main/java/com/axiel7/tioanime/GenreAdapter.java
+++ app/src/main/java/com/axiel7/tioanime/GenreAdapter.java
@@ -11,7 +11,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.ViewHolder> {
+public class GenreAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
     private List<String> mData;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
@@ -21,19 +23,36 @@ public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.ViewHolder> 
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
     }
+    @Override
+    public final int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADER;
+        }
+        return TYPE_ITEM;
+    }
 
     // inflates the row layout from xml when needed
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.genres_row, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == TYPE_ITEM) {
+            view = mInflater.inflate(R.layout.genres_row, parent, false);
+            return new ViewHolder(view);
+        }
+        else if (viewType == TYPE_HEADER) {
+            view = mInflater.inflate(R.layout.drawer_header, parent, false);
+            return new ViewHolderHeader(view);
+        }
+        throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
     }
 
     // binds the data to the TextView in each row
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        String genre = mData.get(position);
-        holder.myTextView.setText(genre);
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ViewHolder) {
+            String genre = mData.get(position);
+            ((ViewHolder) holder).myTextView.setText(genre);
+        }
     }
 
     // total number of rows
@@ -58,6 +77,12 @@ public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.ViewHolder> 
         @Override
         public void onClick(View view) {
             if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+    public class ViewHolderHeader extends RecyclerView.ViewHolder {
+
+        ViewHolderHeader(View itemView) {
+            super(itemView);
         }
     }
 
